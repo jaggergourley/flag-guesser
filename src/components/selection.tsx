e72de04bddf61eebd50df43b0ff58b3f9e4bd995
jaggergourley <jaggergourley@gmail.com>
2024-02-14 19:55:23 -0600

    Beginning dropdown menu for continent selection

--- src/components/selection.tsx
+++ src/components/selection.tsx
@@ -1,5 +1,9 @@
+import React, { useState } from "react";
+import Switch from "./Switch";
+
 export default function Selection() {
   return <p>Select your quiz</p>;
 }
 
-/*this component should handle the logic for the quiz selection*/
+/* this component should handle the logic for the quiz selection*/
+/* based on input (switch and region) it should get only the relevant countries for the quiz*/
